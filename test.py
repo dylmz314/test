0fb3ba6f33dd10193f86359a6c7483b675335a14
dylmz314 <scidurmusyilmaz@gmail.com>
2025-01-06 13:17:42 +0300

    python_1.6.7

--- test.py
+++ test.py
@@ -1,13 +1,1 @@
 print("Helloww")
-print("Hellow2")
-for i in range(10):
-    print(i)
-
-print("Hellow3")
-print("Hellow4")
-#test
-print("Hellow5")
-print("dylmz")
-
-print("saflndslf")
-print("last test")
